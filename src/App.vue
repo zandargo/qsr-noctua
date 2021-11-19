de3c06e60518beec9bd956b78ccb443038084dd1
Madson Germano <madson.br@gmail.com>
2021-11-19 17:15:12 -0300

    WIP

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,13 @@ export default defineComponent({
   name: 'App'
 })
 </script>
+
+<style lang="scss">
+@import "./css/app.scss";
+@import "./css/quasar.variables.scss";
+@font-face {
+  font-family: "RobotoCondensed";
+  src: url("../public/fonts/RobotoCondensed-Regular.ttf") format("truetype");
+  //_ src: url('./assets/fonts/RobotoCondensed-Regular.ttf') format('truetype');
+}
+</style>
